658a8da855e56610f21ac11c38af5f225f99c098
qazmko72 <qazmko72@gmail.com>
2026-06-04 13:00:55 +0800

    fix: revert to clean unsigned IPA build, remove signing attempts

--- scripts/clear_development_team.py
+++
@@ -1,9 +0,0 @@
-import re, sys
-
-path = sys.argv[1]
-with open(path) as f:
-    c = f.read()
-c = re.sub(r'(DEVELOPMENT_TEAM\s*=\s*)[^;]+;', r'\1;', c)
-with open(path, 'w') as f:
-    f.write(c)
-print("DEVELOPMENT_TEAM cleared from", path)
